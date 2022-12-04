bb1e29ddcddd976e90896718fd65a10fd40dec0f
Nhahaha <nahdian7@gmail.com>
2022-12-04 15:06:28 +0700

    implemtasi MVP pada project

--- lib/controller/selected_index.dart
+++ lib/controller/selected_index.dart
@@ -1,59 +1,59 @@
-import 'package:flutter/material.dart';
-import 'package:get/get.dart';
+// import 'package:flutter/material.dart';
+// import 'package:get/get.dart';
 
-class SelectedIndex extends GetxController {
-  RxInt _selectedIndex = 0.obs;
-  RxInt get selectedIndex => _selectedIndex;
+// class SelectedIndex extends GetxController {
+//   RxInt _selectedIndex = 0.obs;
+//   RxInt get selectedIndex => _selectedIndex;
 
-  RxBool _selectedBrandIndex = false.obs;
-  RxBool get selectedBrandIndex => _selectedBrandIndex;
-}
+//   RxBool _selectedBrandIndex = false.obs;
+//   RxBool get selectedBrandIndex => _selectedBrandIndex;
+// }
 
-class SelectedGender extends SelectedIndex {
-  final List<String> _listGender = ["Man", "Woman"].obs;
+// class SelectedGender extends SelectedIndex {
+//   final List<String> _listGender = ["Man", "Woman"].obs;
 
-  List<String> get listGender => _listGender;
+//   List<String> get listGender => _listGender;
 
-  set onTapItem(RxInt value) {
-    _selectedIndex = value;
-    update();
-  }
-}
+//   set onTapItem(RxInt value) {
+//     _selectedIndex = value;
+//     update();
+//   }
+// }
 
-class SelectedBrand extends SelectedIndex {
-  final List<String> _listBrand = [
-    "assets/brand_logo/adidas.png",
-    "assets/brand_logo/new_balance.png",
-    "assets/brand_logo/nike.png",
-    "assets/brand_logo/puma.png",
-    "assets/brand_logo/reebook.png",
-    "assets/brand_logo/umbro.png"
-  ].obs;
+// class SelectedBrand extends SelectedIndex {
+//   final List<String> _listBrand = [
+//     "assets/brand_logo/adidas.png",
+//     "assets/brand_logo/new_balance.png",
+//     "assets/brand_logo/nike.png",
+//     "assets/brand_logo/puma.png",
+//     "assets/brand_logo/reebook.png",
+//     "assets/brand_logo/umbro.png"
+//   ].obs;
 
-  List<String> get listBrand => _listBrand;
+//   List<String> get listBrand => _listBrand;
 
-  set onTapItem(RxBool value) {
-    _selectedBrandIndex = value;
-    update();
-  }
-}
+//   set onTapItem(RxBool value) {
+//     _selectedBrandIndex = value;
+//     update();
+//   }
+// }
 
-class SelectedColor extends SelectedIndex {
-  final List<Color> _listColor = [
-    Colors.blueAccent,
-    Colors.blueGrey,
-    Colors.black,
-  ];
+// class SelectedColor extends SelectedIndex {
+//   final List<Color> _listColor = [
+//     Colors.blueAccent,
+//     Colors.blueGrey,
+//     Colors.black,
+//   ];
 
-  List<Color> get listColor => _listColor;
-}
+//   List<Color> get listColor => _listColor;
+// }
 
-class SelectedPrice extends SelectedIndex {
-  final Rx<RangeValues> _currentRange = RangeValues(10, 300).obs;
+// class SelectedPrice extends SelectedIndex {
+//   final Rx<RangeValues> _currentRange = RangeValues(10, 300).obs;
 
-  Rx<RangeValues> get currentRange => _currentRange;
+//   Rx<RangeValues> get currentRange => _currentRange;
 
-  set currentRange(newValue) {
-    currentRange = newValue;
-  }
-}
+//   set currentRange(newValue) {
+//     currentRange = newValue;
+//   }
+// }
--- lib/main.dart
+++ lib/main.dart
@@ -1,32 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:get/get.dart';
-import 'package:sneak_shoes_app/view/home/component/cart.dart';
-import './immutable.dart';
 
-import 'view/home/home.dart';
-import 'view/product_detail/product_detail.dart';
+import 'app/app.dart';
 
-void main() => runApp(MyApp());
-
-class MyApp extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return GetMaterialApp(
-      title: 'Sneak Shoes App',
-      debugShowCheckedModeBanner: false,
-      getPages: [
-        GetPage(name: '/', page: () => Home()),
-        GetPage(name: '/product-detail', page: () => ProductDetail()),
-        GetPage(name: '/cart', page: () => Cart()),
-      ],
-      theme: ThemeData(
-        scaffoldBackgroundColor: primaryColor,
-        appBarTheme: AppBarTheme(
-          color: Colors.transparent,
-          elevation: 0,
-        ),
-      ),
-      home: Home(),
-    );
-  }
+void main() {
+  runApp(MyApp());
 }
